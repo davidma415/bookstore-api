46899d1524859633d1d020d5cc4545c7563af5d4
David Ma <davidma415@gmail.com>
2019-09-14 15:53:28 -0400

    added error handling for not being able to find records

--- app/controllers/github_webhooks_controller.rb
+++ app/controllers/github_webhooks_controller.rb
@@ -7,36 +7,50 @@ class GithubWebhooksController < ActionController::Base
     randomPublisher = PublishingHouse.order('RANDOM()').first
     case payload['action']
     when "opened"
-      @author = Author.new(name: payload['issue']['title'], biography: payload['issue']['body'])
 
-      @book = Book.new(title: Faker::Lorem.sentence(word_count: 3), author: @author, publisher: randomPublisher, price: 24.20)
+      Author.transaction do
+        @author = Author.create(name: payload['issue']['title'], biography: payload['issue']['body'])
 
-      @author.save
-      @book.save
-
+        Book.transaction do
+          @book = Book.create(title: Faker::Lorem.sentence(word_count: 3), author: @author, publisher: randomPublisher, price: 24.20)
+        end
+      end
     when "deleted"
-      @author = Author.find_by name: payload['issue']['title']
+      @author = find_author(payload['issue']['title'])
       # Destroy object and associated items
       # The reload is needed to update the author's children, and then we can records associated with the author.
-      @author.reload
-      @author.destroy
+      Author.transaction do
+        @author.reload
+        @author.destroy
+      end
     when "edited"
       if payload['changes']['title']
-        @author = Author.find_by name: payload['changes']['title']['from']
+        @author = find_author(payload['changes']['title']['from'])
 
-        @author.update(name: payload['issue']['title'])
+        Author.transaction do
+          @author.update(name: payload['issue']['title'])
+        end
+
       else if payload['changes']['body']
         # Query by the title because the title didn't change
-        @author = Author.find_by name: payload['issue']['title']
+        @author = find_author(payload['issue']['title'])
 
-        @author.update(biography: payload['issue']['body'])
+        Author.transaction do
+          @author.update(biography: payload['issue']['body'])
+        end
       end
     end
   end
 end
 
   private
+  def find_author(name)
+    @author = Author.find_by name: name
+    # Raise an error if we're unable to find an author
+    raise ActiveRecord::RecordNotFound if !@author
 
+    return @author
+  end
   def webhook_secret(payload)
     ENV['GITHUB_WEBHOOK_SECRET']
   end
